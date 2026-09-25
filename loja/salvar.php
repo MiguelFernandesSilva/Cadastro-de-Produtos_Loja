9df2c2fd8a3211b02c1a383dbe63c2c6d357ffed
Miguel Fernandes Silva <miguel.0fersilva0@gmail.com>
2026-09-24 23:37:04 -0300

    Atualizar o salvar.php

--- loja/salvar.php
+++ loja/salvar.php
@@ -4,18 +4,28 @@
         if(file_exists($file)){
             $extract_data = file_get_contents($file);
             $data = json_decode($extract_data, true);
+            if(!is_array($data)){
+                $data = [];
+            }
         }
         return $data;
     }
 
-    function saveData(array $newContact):bool {
+    function saveData(array $newContact, $id = ''):bool {
         $data = readData("dados.json");
-        $data[] = $newContact;
+
+        if($id == ''){
+            $data[] = $newContact;
+        } else {
+            $data[$id] = $newContact;
+        }
+
         $verify = file_put_contents("dados.json", json_encode($data));
         return $verify;
     }
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $id = $_POST['id'] ?? '';
         $name = $_POST['nome'] ?? '';
         $description = $_POST['descricao'] ?? '';
         $value = $_POST['valor'] ?? '';
@@ -26,21 +36,21 @@
             header("location: index.php?msg=Informe o nome e o valor do produto!");
             exit;
         }
-    }
-    
-    $newContact = [
-        "nome" => $name,
-        "descricao" => $description,
-        "valor" => $value,
-        "imagem" => $image,
-        "categoria" => $category
-    ];
 
-    $saved = saveData($newContact);
-        
-    if($saved){
-        header('location: index.php?msg=Produto Cadastrado!');
-    } else {
-        header('location: index.php?msg=Ocorreu um Erro! Tente Novamente!');
+        $newContact = [
+            "nome" => $name,
+            "descricao" => $description,
+            "valor" => $value,
+            "imagem" => $image,
+            "categoria" => $category
+        ];
+
+        $saved = saveData($newContact, $id);
+
+        if($saved){
+            header('location: index.php?msg=Produto Cadastrado!');
+        } else {
+            header('location: index.php?msg=Ocorreu um Erro! Tente Novamente!');
+        }
     }
 ?>
